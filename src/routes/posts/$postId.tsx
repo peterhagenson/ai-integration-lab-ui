e060270831c4ddb376b1945cca294a93be233b17
Peter Hagenson <peterhagenson@Peters-MacBook-Pro.local>
2025-04-05 13:43:44 -0500

    remove duplicate files

--- src/routes/posts/$postId.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/solid-router";
-
-export const Route = createFileRoute("/posts/$postId")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/posts/posts$id"!</div>;
-}
--- src/routes/posts/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/solid-router";
-
-export const Route = createFileRoute("/posts/")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/posts/"!</div>;
-}
